e7148d428b39348fc12e134af6d792403128ec88
Violetta-Yakovleva <lysova-violetta@mail.ru>
2023-06-02 23:57:54 +0500

    HomeWork 4

--- HomeWork4/1.py
+++ HomeWork4/1.py
@@ -12,22 +12,43 @@
 # Input2: 3 9 12 15 18
 # Output: Повторяющихся чисел нет
 
+
+# Раскомментировать для проверки первую пару или вторую
+
 # numbers_set1 = [2, 4, 6, 8, 10, 12, 10, 8, 6, 4, 2]
 # numbers_set2 = [3, 6, 9, 12, 15, 18]
 
-numbers_set1 = [2, 4, 6, 8, 10, 10, 8, 6, 4, 2]
-numbers_set2 = [3, 9, 12, 15, 18]
+# numbers_set1 = [2, 4, 6, 8, 10, 10, 8, 6, 4, 2]
+# numbers_set2 = [3, 9, 12, 15, 18]
 
-finish_set = []
-new_finish_set = ()
 
-for i in numbers_set1:
-    if i in numbers_set2:
-        finish_set.append(i)
-        new_finish_set = set(finish_set)
-        new_finish_set = list(new_finish_set)
-        new_finish_set.sort()
-print(*new_finish_set)
+# Первый вариант
+
+# finish_set = []
+# new_finish_set = ()
+#
+# for i in numbers_set1:
+#     if i in numbers_set2:
+#         finish_set.append(i)
+#         new_finish_set = set(finish_set)
+#         new_finish_set = list(new_finish_set)
+#         new_finish_set.sort()
+# print(*new_finish_set)
+
+
+# Второй вариант
+
+# numbers_set1 = set(numbers_set1)
+# numbers_set2 = set(numbers_set2)
+#
+# new_set = numbers_set1.intersection(numbers_set2)
+# new_set = list(new_set)
+# new_set.sort()
+#
+# if new_set == []:
+#     print('Повторяющихся чисел нет')
+# else:
+#     print(*new_set)
 
 
 
--- HomeWork4/2.py
+++ HomeWork4/2.py
@@ -12,4 +12,34 @@
 # Input1: 11, 92, 1, 42, 15, 12, 11, 81
 # Output1: Макс. кол-во ягод 184, собрано для куста 1
 
-bushes = int(input("Введите количество кустов: "))
+# bushes = int(input("Введите количество кустов: "))
+# number_berries = []
+
+# for i in range(bushes):
+#     number_berries.append(int(input(f'Введите количество ягод на {i+1} кусте: ')))
+# print(number_berries)
+
+bushes = 8
+num_berries = [11, 92, 1, 42, 15, 12, 11, 81]
+
+for i in range(len(num_berries)-1):
+    max_sum = 0
+    bush = 0
+
+    if i == 0:
+        berries_sum = num_berries[i] + num_berries[i+1] + num_berries[len(num_berries)-1]
+        if berries_sum > max_sum:
+            max_sum = berries_sum
+            bush = i
+    if i == (len(num_berries)-1):
+        berries_sum = num_berries[i] + num_berries[i-1] + num_berries[0]
+        if berries_sum > max_sum:
+            max_sum = berries_sum
+            bush = i
+    else:
+        berries_sum = num_berries[i] + num_berries[i-1] + num_berries[i+1]
+        if berries_sum > max_sum:
+            max_sum = berries_sum
+            bush = i
+
+print(f'Максимальное количество ягод {max_sum} будет собрано с {i+1} куста')
