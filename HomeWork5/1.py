f46865f814a2e605785f54dbc5bddb0ab67ebd1a
Violetta-Yakovleva <lysova-violetta@mail.ru>
2023-06-05 15:50:27 +0500

    HomeWork 5

---
+++ HomeWork5/1.py
@@ -0,0 +1,17 @@
+# Задача 1. Напишите рекурсивную функцию для возведения числа a в степень b.
+# Разрешается использовать только операцию умножения. Циклы использовать нельзя
+# Примеры/Тесты:
+# <function_name>(2,0) -> 1
+# <function_name>(2,1) -> 2
+# <function_name>(2,3) -> 8
+# <function_name>(2,4) -> 16
+
+
+# number = int(input('Введите число для возведения в степень: '))
+# extent_number = int(input('Введите степень: '))
+
+# def number_power (number: int, extent_number: int) -> int:
+#     if extent_number == 0:
+#         return 1
+#     return number * number_power(number, extent_number - 1)
+# print(number_power(number, extent_number))
